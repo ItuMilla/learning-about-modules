c960c3d54b9d9a8d867c6ddcfb58201f5a6788f5
ItuMilla <itumelenglephondo94@gmail.com>
2022-08-22 13:02:05 +0200

    git commit -m "index.js"

--- index.js
+++ index.js
@@ -10,11 +10,12 @@ const riddle =fun =fun.getRandomRiddle();
 console.log(riddle.riddle);
 // show the answer to the riddle after 3 seconds
 setTimeout(function() {
-    console.log(riddle.answer);
+    console.log(chalk.bgBlackBright.bold(riddle.answer));
 }, 3000)
 
 
 
 //console.log(chalk.bgBlackBright.bold{greet{'Itumeleng')});
+
 // console.log(greet('Itumeleng'));
 // console.log(greet('Xola'));
