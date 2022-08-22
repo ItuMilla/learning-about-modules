c1477b34331e2febbd80928b508636eafde9bd1c
ItuMilla <itumelenglephondo94@gmail.com>
2022-08-22 12:44:41 +0200

    git commit -m "index.js"

---
+++ index.js
@@ -0,0 +1,20 @@
+// acts like a "script" tag ..
+
+import greet from './greet.js';
+import chalk from 'chalk';
+
+import fun from "everyday-fun";
+
+const riddle =fun =fun.getRandomRiddle();
+
+console.log(riddle.riddle);
+// show the answer to the riddle after 3 seconds
+setTimeout(function() {
+    console.log(riddle.answer);
+}, 3000)
+
+
+
+//console.log(chalk.bgBlackBright.bold{greet{'Itumeleng')});
+// console.log(greet('Itumeleng'));
+// console.log(greet('Xola'));
