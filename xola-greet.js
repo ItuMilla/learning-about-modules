bbb60311e97ca825067e2d46450bcd59a45be123
ItuMilla <itumelenglephondo94@gmail.com>
2022-08-22 12:52:53 +0200

    git commit -m "xola-greet.js"

--- xola-greet.js
+++ xola-greet.js
@@ -1,6 +1,7 @@
-import chalk from 'figlet';
+import chalk from 'chalk';
 //import the greet module that is in the current folder
 import greet from './greet.js'
 
+// call the function and print the result on the screen
 const styledMessage = chalk.bgGreen.black(greet('Xola'));
 console.log(styledMessage)
